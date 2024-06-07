4c440f8f3e7931a98aa36f2b70dfbf634bcafcdc
sinnnny23 <129198975+sinnnny23@users.noreply.github.com>
2024-06-07 19:29:57 +0900

    Update git.java

--- src/gitPractice/git.java
+++ src/gitPractice/git.java
@@ -3,5 +3,6 @@ package gitPractice;
 public class git {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!!");
+        System.out.println("fetch 테스트!!");
     }
 }
