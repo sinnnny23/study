cb3032ad72537665977789068598b61a365f7f8f
sinnnny23 <kts0223@naver.com>
2024-06-09 22:03:04 +0900

    git practice2

--- src/gitPractice/git.java
+++ src/gitPractice/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!!");
         System.out.println("fetch 테스트!!");
+        System.out.println("rebase 테스트!!");
     }
 }
